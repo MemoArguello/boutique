1073036d513766eadf87133c5537a9914584c275
Guillermo Argüello Cordobés <74156373+MemoArguello@users.noreply.github.com>
2025-02-11 15:52:06 -0300

    Update login.php

--- Boutique/frontend/login.php
+++ Boutique/frontend/login.php
@@ -38,36 +38,47 @@ $productos = $consulta->fetchAll(PDO::FETCH_OBJ);
   <!-- Unicons -->
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
   <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
   <!-- Header -->
+  <!-- Header -->
   <header class="header">
     <nav class="nav">
-      <a href="#" class="nav_logo"><?= $datos->nombre_empresa ?></a>
-      <ul class="nav_items">
+      <div class="nav_logo">
+        <img src="../backend/controladores/<?= $datos->imagen_url ?>" alt="" class="logo_img">
+        <a href="#" class="nav_logo_text"><?= $datos->nombre_empresa ?></a>
+      </div>
+
+      <button class="menu-toggle" id="menu-toggle">
+        ☰
+      </button>
+
+      <ul class="nav_items" id="nav-menu">
+        <li class="nav_item"><a href="#" class="nav_link">Inicio</a></li>
+        <li class="nav_item"><a href="#productos" class="nav_link">Productos</a></li>
+        <li class="nav_item"><a href="#" class="nav_link">Información</a></li>
+        <li class="nav_item"><a href="#" class="nav_link">Contacto</a></li>
         <li class="nav_item">
-          <a href="#" class="nav_link">Home</a>
-          <a href="#" class="nav_link">Product</a>
-          <a href="#" class="nav_link">Services</a>
-          <a href="#" class="nav_link">Contact</a>
+          <button class="button" id="form-open">Iniciar Sesión</button>
         </li>
       </ul>
-      <button class="button" id="form-open">Iniciar Sesión</button>
     </nav>
   </header>
+
   <!-- Home -->
   <section class="home">
-  <div class="flex flex-col items-center justify-center h-screen text-center">
-  <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-      <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-200">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-      <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-        Learn more
-        <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-        </svg>
+
+    <div class="titulo-home">
+      <h1 class="titulo-grande"><?= $datos->nombre_empresa ?></h1>
+      <p class="bg-black bg-opacity-30 mb-6 text-lg font-normal text-gray-200 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-200">Descubrí las últimas tendencias en moda con prendas únicas, tenemos todo lo que necesitás para lucir increíble en cualquier ocasión.</p>
+      <a href="#productos" type="button" class="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2">
+        Ver Catalogo
       </a>
     </div>
+
 
     <div class="form_container">
       <i class="uil uil-times form_close"></i>
@@ -114,26 +125,81 @@ $productos = $consulta->fetchAll(PDO::FETCH_OBJ);
     </div>
 
   </section>
-  <section class="products">
-    <h2 class="section_title">Nuestros Productos</h2>
+  <section class="products" id="productos">
+    <h2 class="titulo-segundo">Mas Vendidos</h2>
     <div class="products_container">
-    <?php foreach ($productos as $producto): ?>
+      <?php foreach ($productos as $producto): ?>
 
-      <div class="product_card">
-        <img src="../backend/controladores/<?=$producto->imagen_url ?>" alt="Producto 1">
-        <h3><?= $producto->nombre ?></h3>
-        <p class="price">$15.99</p>
-        <button class="button">Agregar al carrito</button>
-      </div>
+        <div class="product_card">
+          <img src="../backend/controladores/<?= $producto->imagen_url ?>" alt="Producto 1">
+          <h3><?= $producto->nombre ?></h3>
+          <p class="price"><?= number_format($producto->precio, 0, ',', '.') ?> Gs.</p>
+          <button class="button">Agregar al carrito</button>
+        </div>
 
       <?php endforeach; ?>
 
     </div>
   </section>
 
-  <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.js"></script>
+  <section class="products" id="productos">
+
+    <h2 class="titulo-segundo">Nuestros Productos</h2>
+    <div class="products_container">
+      <?php foreach ($productos as $producto): ?>
+
+        <div class="product_card">
+          <img src="../backend/controladores/<?= $producto->imagen_url ?>" alt="Producto 1">
+          <h3><?= $producto->nombre ?></h3>
+          <p class="price"><?= number_format($producto->precio, 0, ',', '.') ?> Gs.</p>
+          <button class="button">Agregar al carrito</button>
+        </div>
+
+      <?php endforeach; ?>
+
+    </div>
+  </section>
+  <section class="home">
+    <div class="titulo-home">
+
+      <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Get back to growth with <span class="text-blue-600 dark:text-blue-500">the world's #1</span> CRM.</h1>
+      <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+    </div>
+  </section>
+
 
   <script src="js/script.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.js"></script>
+  <script>
+    document.getElementById("menu-toggle").addEventListener("click", function() {
+      document.getElementById("nav-menu").classList.toggle("active");
+    });
+  </script>
 </body>
+
+<footer>
+    <div class="footerContainer">
+        <div class="socialIcons">
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-twitter"></i></a>
+            <a href=""><i class="fa-brands fa-google-plus"></i></a>
+            <a href=""><i class="fa-brands fa-youtube"></i></a>
+        </div>
+        <div class="footerNav">
+            <ul><li><a href="">Inicio</a></li>
+                <li><a href="">Información</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Contactos</a></li>
+            </ul>
+        </div>
+        
+    </div>
+    <div class="footerBottom">
+        <p>Copyright &copy;2025; Diseñado por <span class="designer">Nicoletta</span></p>
+    </div>
+</footer>
+
+
 
 </html>
